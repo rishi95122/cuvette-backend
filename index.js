57045abdb66005c53b464c6d8e9503466033819a
rishi95122 <rishi95122@gmail.com>
2024-10-19 14:44:55 +0530

    added notification

--- index.js
+++ index.js
@@ -1,28 +1,21 @@
 import express from 'express';
-
 import dotenv from 'dotenv';
 import cors from 'cors';
-
 import authRoutes from './routes/auth.routes.js';
 import jobRoutes from './routes/job.routes.js';
 import emailRoutes from './routes/email.routes.js';
 import connectDb from './mongo/mongo.js';
 
 dotenv.config();  
-
 const app = express();
 app.use(express.json());
 const options={
   origin:"https://cuvette-frontend-five.vercel.app",
 }
-app.use(cors());
-
-
+app.use(cors(options));
 app.use('/api/auth', authRoutes);
 app.use('/api/jobs', jobRoutes);
 app.use('/api/email', emailRoutes);
-
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     connectDb()
